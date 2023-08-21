cfbbd157190415ad861746fbdfbfe44e7b5c84a3
MoamenRamy <moamen.ramy2000@gmail.com>
2023-08-21 11:44:18 +0300

    database, migrations, relations

---
+++ app/Models/Video.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Video extends Model
+{
+    use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class); // 1 video pertama kali dibuat oleh user yang bernilai
+    }
+
+    public function convertedvideos()
+    {
+        return $this->hasMany(Convertedvideo::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function views()
+    {
+        return $this->hasMany(View::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(Video::class, 'video_user', 'video_id', 'user_id')->withTimestamps()->withPivot('id');
+    }
+}
